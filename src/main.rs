98046f4ec13252991f979d9d74c4b18b1422b79c
Kaporos <theo@daron.be>
2022-10-15 16:12:32 +0200

    chore: 🔧 Changed release profile for binary size optimisations

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,5 @@ use std::fs;
 fn main() {
     let content = fs::read_to_string("warrior.rc").expect("Can't read file !");
     let code = parser::parse(&content);
-    println!("{:#?}", code)
+    println!("{:#?}", code);
 }
