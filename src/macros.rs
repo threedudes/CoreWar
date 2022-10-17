59050f030e6c804b8d03da412fdf0b9edf48b899
Kaporos <theo@daron.be>
2022-10-17 17:19:14 +0200

    refactor: ♻️ Changed a little bit macro to show enum we're talking about inside not found error

--- src/macros.rs
+++ src/macros.rs
@@ -1,17 +1,17 @@
 #[macro_export]
 macro_rules! matcher_gen {
     (enum $name:ident {
-        $($opstring:literal => $instruction:ident),*
+        $($opstring:literal => $variant:ident),*
     }) => {
         #[derive(Clone, Debug)]
         pub enum $name {
-            $($instruction,)*
+            $($variant,)*
         }
         impl $name {
             pub fn from_str(string: &str) -> Result<$name, std::io::Error> {
                 match string {
-                    $($opstring => Ok($name::$instruction),)*
-                    _ => Err(std::io::Error::new(std::io::ErrorKind::InvalidData, format!("{} instruction not found !", string)))
+                    $($opstring => Ok($name::$variant),)*
+                    _ => Err(std::io::Error::new(std::io::ErrorKind::InvalidData, format!("'{}' equivalent variant not found in enum {}", string, stringify!($name))))
                 }
             }
         }
